5426de8b58a6d97634289f730cabed2a4225de14
kazirauf <raufelahi9876@gmail.com>
2024-07-12 19:44:50 +0600

    Our Pure Petals Gallery section is done

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -8,77 +8,21 @@ const Gallery = () => {
   return (
     <section className="bg-[#EEEDEB] pb-20">
       <h2 className="text-2xl text-center font-bold text-gray-800 lg:text-4xl dark:text-white mb-8">
-        Explore our <span className="text-[#508D4E]">Image Gallery</span>
+      <span className="text-[#75ef71]">Our Pure Petals Gallery</span>
       </h2>
 
-      <div className="py-4 px-2 mx-auto max-w-screen-xl sm:py-4 lg:px-6">
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 gap-4 h-full">
-          <div className="col-span-2 sm:col-span-1 md:col-span-2 bg-gray-50 h-auto md:h-full flex flex-col">
-            <div className="group relative flex flex-col overflow-hidden rounded-lg px-4 pb-4 pt-40 flex-grow">
-              <img
-                src={galleryOne}
-                alt=""
-                className="absolute inset-0 h-full w-full object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out"
-              />
-              <div className="absolute inset-0 bg-gradient-to-b from-gray-900/25 to-gray-900/5"></div>
-              <h3 className="z-10 bg-[#508D4E] text-2xl font-medium text-white absolute top-0 left-0 p-2 xs:text-xl md:text-xl rounded-br-lg">
-                Oak Tree
-              </h3>
-            </div>
-          </div>
-
-          <div className="col-span-2 sm:col-span-1 md:col-span-2 bg-stone-50">
-            <div className="group relative flex flex-col overflow-hidden rounded-lg px-4 pb-4 pt-40 mb-4">
-              <img
-                src={galleryTwo}
-                alt=""
-                className="absolute inset-0 h-full w-full object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out"
-              />
-              <div className="absolute inset-0 bg-gradient-to-b from-gray-900/25 to-gray-900/5"></div>
-              <h3 className="z-10 bg-[#508D4E] text-2xl font-medium text-white absolute top-0 left-0 p-2 xs:text-xl md:text-xl rounded-br-lg">
-                Maple Tree
-              </h3>
-            </div>
-            <div className="grid gap-4 grid-cols-2 sm:grid-cols-2 lg:grid-cols-2">
-              <div className="group relative flex flex-col overflow-hidden rounded-lg px-4 pb-4 pt-40">
-                <img
-                  src={galleryThree}
-                  alt=""
-                  className="absolute inset-0 h-full w-full object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out"
-                />
-                <div className="absolute inset-0 bg-gradient-to-b from-gray-900/25 to-gray-900/5"></div>
-                <h3 className="z-10 bg-[#508D4E] text-2xl font-medium text-white absolute top-0 left-0 p-2 xs:text-xl md:text-xl rounded-br-lg">
-                  Pine Tree
-                </h3>
-              </div>
-              <div className="group relative flex flex-col overflow-hidden rounded-lg px-4 pb-4 pt-40">
-                <img
-                  src={galleryFour}
-                  alt=""
-                  className="absolute inset-0 h-full w-full object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out"
-                />
-                <div className="absolute inset-0 bg-gradient-to-b from-gray-900/25 to-gray-900/5"></div>
-                <h3 className="z-10 bg-[#508D4E] text-2xl font-medium text-white absolute top-0 left-0 p-2 xs:text-xl md:text-xl rounded-br-lg">
-                  Sequioa Tree
-                </h3>
-              </div>
-            </div>
-          </div>
-          <div className="col-span-2 sm:col-span-1 md:col-span-1 bg-sky-50 h-auto md:h-full flex flex-col">
-            <div className="group relative flex flex-col overflow-hidden rounded-lg px-4 pb-4 pt-40 flex-grow">
-              <img
-                src={galleryFive}
-                alt=""
-                className="absolute inset-0 h-full w-full object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out"
-              />
-              <div className="absolute inset-0 bg-gradient-to-b from-gray-900/25 to-gray-900/5"></div>
-              <h3 className="z-10 bg-[#508D4E] text-2xl font-medium text-white absolute top-0 left-0 p-2 xs:text-xl md:text-xl rounded-br-lg">
-                Eucalyptus Tree
-              </h3>
-            </div>
-          </div>
-        </div>
-      </div>
+  <div className="flex justify-center">
+  <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-10">
+      <img className="w-96 h-70" src="https://images.unsplash.com/photo-1463088255606-4848481faffb?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+      <img className="w-96 h-70" src="https://images.unsplash.com/photo-1702207563713-003728a8fb68?q=80&w=1973&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+      <img className="w-96 h-70" src="https://images.unsplash.com/photo-1462143338528-eca9936a4d09?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+      <img className="w-96 h-70" src="https://images.unsplash.com/reserve/bOvf94dPRxWu0u3QsPjF_tree.jpg?q=80&w=2076&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+      <img className="w-96 h-70" src="https://images.unsplash.com/reserve/unsplash_5247177ff41f4_1.JPG?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+      <img className="w-96 h-70" src="https://images.unsplash.com/photo-1457337771881-93c35845fd4a?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+      <img className="w-96 h-70" src="https://images.unsplash.com/photo-1597170558584-498bc3a3fe80?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+      <img className="w-96 h-70" src="https://images.unsplash.com/photo-1522223985102-715eccbf0b54?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+    </div>
+  </div>
     </section>
   );
 };
